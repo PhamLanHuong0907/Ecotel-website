efb7967438d8d7d0d35bfe7be5ffb67a7c5fa621
PhamLanHuong0907 <huongphamlan0907@gmail.com>
2026-05-08 14:01:09 +0700

    Update path new

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Leadership from "./pages/Leadership";
 import Customers from "./pages/Customer";
 import ScrollToTop from "./components/Component_mini/Scrolltotop";
 // [NEW] Import ThemeToggle
-import ThemeToggle from "./components/ThemeToogle"; 
+import ThemeToggle from "./components/ThemeToogle";
 import Award from "./pages/Prize_page";
 import ServicePage from "./pages/ServicePage";
 import ProductsPage from "./pages/ProductsPage";
@@ -22,18 +22,18 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-   
+
       <Toaster />
       <Sonner />
-      
+
       <BrowserRouter>
-        <ScrollToTop/>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/gioi-thieu-chung" element={<About />} />
           <Route path="/doi-ngu-lanh-dao" element={<Leadership />} />
           <Route path="/khach-hang" element={<Customers />} />
-          <Route path="/giai-thuong" element={<Award/>}/>
+          <Route path="/giai-thuong" element={<Award />} />
           <Route path="*" element={<NotFound />} />
           <Route path="/:serviceSlug" element={<ServicePage />} />
           <Route path="/:serviceSlug/:productSlug" element={<ProductsPage />} />
@@ -45,7 +45,7 @@ const App = () => (
         {/* [NEW] Đặt nút đổi màu ở góc màn hình. 
             z-50 để nổi lên trên cùng, fixed để luôn hiển thị khi cuộn */}
         <div className="fixed bottom-6 left-6 z-50 md:bottom-10 md:left-10 animate-fade-up">
-           <ThemeToggle />
+          <ThemeToggle />
         </div>
 
       </BrowserRouter>
